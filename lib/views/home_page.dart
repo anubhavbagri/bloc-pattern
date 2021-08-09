52c747343a644a702ac0cafea61a607a2b2901a7
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-09 19:16:42 +0530

    feat: finish techcrunch new app UI

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:bloc_pattern/models/news_info.dart';
+import 'package:bloc_pattern/services/api_manager.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +10,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Future<Welcome>? _newsModel;
+
+  @override
+  void initState() {
+    _newsModel = APIManager().getNews();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,12 +25,56 @@ class _HomePageState extends State<HomePage> {
         title: Text('News App'),
       ),
       body: Container(
-        child: ListView.builder(itemBuilder: (context, index) {
-          return Container(
-            height: 100,
-            color: Colors.red,
-          );
-        }),
+        child: FutureBuilder<Welcome>(
+          future: _newsModel,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.articles.length,
+                  itemBuilder: (context, index) {
+                    var article = snapshot.data!.articles[index];
+                    return Column(
+                      children: [
+                        ListTile(
+                          title: Text(
+                            article.title,
+                            style:
+                                Theme.of(context).textTheme.bodyText2!.copyWith(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                          ),
+                          subtitle: Text(
+                            article.description,
+                            style:
+                                Theme.of(context).textTheme.caption!.copyWith(
+                                      color: Colors.black,
+                                    ),
+                          ),
+                          leading: ConstrainedBox(
+                            constraints: BoxConstraints(
+                              minWidth: 44,
+                              minHeight: 64,
+                              maxWidth: 64,
+                              maxHeight: 64,
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.network(
+                                article.urlToImage,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  });
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
     );
   }
